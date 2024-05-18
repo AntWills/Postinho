3f85f975265e0f1d58e7439011f8415560f4e06a
Antonio Wills <antoniowillissilvasantos@gmail.com>
2024-05-18 16:03:01 -0300

    Construção da class ReadData e Pacient

---
+++ src/Patient/Patient.java
@@ -0,0 +1,12 @@
+package Patient;
+
+public class Patient {
+    int id, age;
+    String name;
+
+    public Patient(int id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+}
---
+++ src/Read/ReadData.java
@@ -0,0 +1,23 @@
+package Read;
+
+import java.util.Scanner;
+
+public class ReadData {
+    private static Scanner input = new Scanner(System.in);
+
+    public static int INT() {
+        int aux = input.nextInt();
+        input.nextLine();
+        return aux;
+    }
+
+    public static double DOUBLE() {
+        double aux = input.nextDouble();
+        input.nextLine();
+        return aux;
+    }
+
+    public static String STRING() {
+        return input.nextLine();
+    }
+}
